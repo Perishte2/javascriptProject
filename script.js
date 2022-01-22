589d8e251b2c915bc47455f6f8fddf8b49ef774b
Perishte <iperishte@bk.ru>
2022-01-22 10:32:52 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+//Variables
+
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+
+
+
+//Data types
+
+var number = 5;
+var string = "Hello!";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4/0);
+//console.log(string/9);
+//when variable does not exist it gives a null result
+//console.log(something);
+//when variable exists but does not have a result it is undefined
+let something;
+console.log(something);
+
+let person = {name:"John",
+age: 25,
+isMarried: false};
+
+
+console.log(person.name);
+console.log(person["age"]);
+
+let arr = ["apple.png","plum.png","orange.png"];
+console.log(arr[2]);
---
+++ script2.js
@@ -0,0 +1,42 @@
+'use strict';   //we are using ES6
+
+let a=5;
+console.log(a);
+
+//alert("Hello World!");
+
+//gives boolean result true or false
+//let answer = confirm("Are you here?");
+//console.log(answer);
+//gives an answer
+//let answer = prompt("Are you 18?");
+//console.log(answer);
+
+
+//operatory
+//concatenation
+//console.log("arr" + "-object");
+
+//+ turns string into number
+//let age = prompt("Are you 18? "," yes");
+//console.log(typeof(age));
+
+
+//increment and decrement
+/* let incr = 10;
+let decr = 10;
+
+console.log(incr++);
+console.log(decr--);  */
+
+//console.log(5%2);
+
+//strict equal and not strict equal
+
+//.log("2" == 2);
+//console.log("2" === 2);
+
+
+//commits in git are used for tracking for creating control points
+//after adding files into git it tracks after all added files
+
